568044e2382807e593a622b43fb4cbe74eff5c1a
David Tang <dev@blueur.com>
2025-02-22 18:36:05 +0100

    feat: add test + ci-cd

--- src/pages/slides/ci-cd.tsx
+++ src/pages/slides/ci-cd.tsx
@@ -1,4 +1,5 @@
-import { Section } from "@site/src/components/Deck";
+import { Code, Image, List, Section, Text } from "@site/src/components/Deck";
+import Mermaid from "@site/src/components/Mermaid";
 import RevealDeck from "@site/src/components/RevealDeck";
 
 export default function CiCd(props: { embedded?: boolean }) {
@@ -9,7 +10,288 @@ export default function CiCd(props: { embedded?: boolean }) {
       title="CI/CD"
       subTitle="DevOps"
     >
-      <Section level={2} title="CI/CD"></Section>
+      <Section level={3} title="CI/CD ?">
+        <List
+          fragment
+          items={[
+            "_CI_: Continuous **Integration** (Intégration continue)",
+            "_CD_: Continuous **Delivery** (Distribution/Livraison continue)",
+            "_CD_: Continuous **Deployment** (Déploiement continu)",
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Continuous Integration">
+        <List
+          fragment={[false, true, true]}
+          items={[
+            [
+              "Problèmes",
+              [
+                "**Plusieurs** développeurs sur le même projet",
+                "**Intégration** des modifications dans un code **commun**",
+                "Intégration des **services**",
+              ],
+            ],
+            [
+              "Solution",
+              [
+                [
+                  "**Automatiser** l'intégration",
+                  ["La **construction/compilation** (build)", "Les **tests**"],
+                ],
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Continuous Delivery">
+        <List
+          fragment={[true, true]}
+          items={[
+            [
+              "**Automatiser** la **distribution** du logiciel",
+              [
+                "Création de l'**artefact** (jar, Docker image, etc.)",
+                "**Publication** de l'artefact (npm, pip, Docker Hub, etc.)",
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Continuous Deployment">
+        <List
+          fragment={[true, true]}
+          items={[
+            [
+              "**Automatiser** le **déploiement** du logiciel",
+              [
+                "**Installation** de l'artéfact sur l'environnement de **production**",
+                "**Mise à jour** du logiciel",
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={3} title="CI/CD">
+        <Image
+          src="https://www.redhat.com/rhdc/managed-files/styles/wysiwyg_full_width/private/ci-cd-flow-desktop.png.webp?itok=mDEvsSsp"
+          url="https://www.redhat.com/fr/topics/devops/what-is-ci-cd"
+        />
+      </Section>
+      <Section level={3} title="CI/CD">
+        <Image
+          src="https://wac-cdn.atlassian.com/dam/jcr:b2a6d1a7-1a60-4c77-aa30-f3eb675d6ad6/ci%20cd%20asset%20updates%20.007.png"
+          url="https://www.atlassian.com/continuous-delivery/principles/continuous-integration-vs-delivery-vs-deployment"
+        />
+      </Section>
+      <Section level={3} title="CI/CD">
+        <Mermaid>
+          {`
+          sequenceDiagram
+            actor dev as Developer
+            participant vcs as Version Control System<br>(Git)
+            participant cis as Continuous Integration Tool<br>(Jenkins, GitLab CI, ...)
+            participant ar as Artifact Repository<br>(Sonatype Nexus, Artifactory, ...)
+            participant paas as Platform as a Service<br>(Kubernetes, AWS, ...)
+
+            autonumber
+
+            rect rgb(227, 242, 253)
+            note right of dev: Continuous Integration
+            dev->>vcs: push code source
+            vcs->>cis: trigger build
+            cis->>vcs: pull code source
+            cis->>cis: build & test
+            end
+
+            rect rgb(255, 235, 238)
+            note right of cis: Continuous Delivery
+            cis->>cis: build artifact
+            cis->>ar: publish artifact
+            end
+
+            rect rgb(232, 245, 233)
+            note right of cis: Continuous Deployment
+            cis->>paas: trigger deployment
+            paas->>ar: pull artifact
+            paas->>paas: deploy artifact
+            end
+          `}
+        </Mermaid>
+      </Section>
+      <Section level={2} title="Outils">
+        <List
+          fragment
+          items={[
+            [
+              "[Jenkins](https://www.jenkins.io/)",
+              [
+                "Open source & on-premise",
+                "Jobs lancés sur un serveur (maintenant aussi possible sur un container)",
+              ],
+            ],
+            [
+              "[GitHub Actions](https://github.com/features/actions)",
+              "Intégré à GitHub",
+            ],
+            [
+              "[GitLab CI/CD](https://docs.gitlab.com/ee/ci/)",
+              ["Intégré à GitLab"],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={2} title="GitLab CI/CD">
+        <Image
+          src="https://docs.gitlab.com/ci/quick_start/img/pipeline_graph_v17_9.png"
+          url="https://docs.gitlab.com/ci/quick_start/"
+          height="50%"
+        />
+        <List
+          fragment={[true, true, true]}
+          items={[
+            [
+              "_Pipeline_",
+              [
+                [
+                  "_Stage_ (build, test, deploy, etc.)",
+                  [
+                    "_Job_ contient un **script** exécuté par un **runner** dans un **container**",
+                  ],
+                ],
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={3} title="`.gitlab-ci.yml`">
+        <Code>
+          {`
+          build-job:
+            stage: build
+            script:
+              - echo "Hello, $GITLAB_USER_LOGIN!"
+
+          test-job1:
+            stage: test
+            script:
+              - echo "This job tests something"
+
+          test-job2:
+            stage: test
+            script:
+              - echo "This job tests something, but takes more time than test-job1."
+              - echo "After the echo commands complete, it runs the sleep command for 20 seconds"
+              - echo "which simulates a test that runs 20 seconds longer than test-job1"
+              - sleep 20
+
+          deploy-prod:
+            stage: deploy
+            script:
+              - echo "This job deploys something from the $CI_COMMIT_BRANCH branch."
+            environment: production
+          `}
+        </Code>
+      </Section>
+      <Section level={3} title="Job et stage">
+        <List
+          fragment={[false, true]}
+          items={[
+            [
+              "**Job** définit **quoi** exécuter (script)",
+              ["Condition avec `rules`", "Dépendance avec `needs`"],
+            ],
+            [
+              "**Stage** définit **quand** exécuter un job",
+              [
+                [
+                  "**Ordre** des stages (par défaut)",
+                  ["`.pre` > `build` > `test` > `deploy` > `.post`"],
+                ],
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Variables">
+        <List
+          fragment={[true, true]}
+          items={[
+            [
+              "**Variables d'environnement** injectées dans les jobs",
+              [
+                "`CI_COMMIT_BRANCH`,`CI_COMMIT_REF_NAME`, `CI_PIPELINE_SOURCE`, etc.",
+                "[Predefined variables reference](https://docs.gitlab.com/ee/ci/variables/predefined_variables.html)",
+              ],
+            ],
+            "Définition de ses propres variables",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Trigger une pipeline">
+        <List
+          fragment
+          items={[
+            "Sur un **push** sur une branche",
+            "Sur une **merge request**",
+            "**Régulièrement** (cron)",
+            "**Webhook** sur l'API",
+            "**Manuellement** depuis l'interface",
+            "Depuis une **autre pipeline**",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Artefacts">
+        <List
+          fragment
+          items={[
+            "Fichiers ou dossiers",
+            "À la fin de chaque **job**, possibilité de **sauvegarder** des **artefacts**",
+            "Possibilité de **récupérer** les artéfacts d'un job dans un autre",
+            "Possibilité de **télécharger** les artéfacts depuis l'interface",
+            "[Job artifacts](https://docs.gitlab.com/ee/ci/jobs/job_artifacts.html)",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Cache">
+        <List
+          fragment
+          items={[
+            [
+              "Possibilité de **cacher** des fichiers ou dossiers entre les **exécutions** d'un même job",
+              ["[Caching](https://docs.gitlab.com/ee/ci/caching/)"],
+            ],
+            "Accélère les **exécutions** des jobs",
+            "Utilisé pour les **dépendances** (node_modules, .m2, etc.)",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="DRY">
+        <List
+          fragment
+          items={[
+            "DRY = Don't Repeat Yourself",
+            "Pour **réutiliser** des pipelines",
+            "[extends](https://docs.gitlab.com/ee/ci/yaml/#extends) pour réutiliser un job (de la même pipeline)",
+            "[YAML anchors](https://docs.gitlab.com/ee/ci/yaml/yaml_optimization.html#anchors)",
+            "[include](https://docs.gitlab.com/ee/ci/yaml/#include) pour réutiliser un fichier externe",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Runner">
+        <List
+          fragment
+          items={[
+            "Un **agent** qui exécute les jobs sur un serveur",
+            ["**SaaS** runners sur GitLab.com", ["Limité puis payant"]],
+            ["**Self-managed** runners", ["Illimité mais à gérer"]],
+            "[GitLab Runner](https://docs.gitlab.com/runner/)",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Référence">
+        <Text>https://docs.gitlab.com/ee/ci/yaml/</Text>
+      </Section>
     </RevealDeck>
   );
 }
--- src/pages/slides/test.tsx
+++ src/pages/slides/test.tsx
@@ -1,4 +1,4 @@
-import { Section } from "@site/src/components/Deck";
+import { Image, List, Section } from "@site/src/components/Deck";
 import RevealDeck from "@site/src/components/RevealDeck";
 
 export default function Test(props: { embedded?: boolean }) {
@@ -9,7 +9,217 @@ export default function Test(props: { embedded?: boolean }) {
       title="Test automatisé"
       subTitle="DevOps"
     >
-      <Section level={2} title="Test automatisé"></Section>
+      <Section level={3} title="Tester c'est douter">
+        <Image
+          src="https://live.staticflickr.com/4500/37150757250_00a4a2a533_c.jpg"
+          url="https://www.flickr.com/photos/xapn/37150757250"
+        />
+      </Section>
+      <Section level={3} title="Pourquoi ?">
+        <List
+          fragment
+          items={[
+            "Éviter les **bugs** et les **régressions**",
+            "Vérifier la **sécurité** et la **performance**",
+            "Valider les **spécifications**",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Quand ?">
+        <Image
+          src="https://www.researchgate.net/profile/Maurice-Dawson/publication/255965523/figure/fig1/AS:297976107945984@1448054448271/IBM-System-Science-Institute-Relative-Cost-of-Fixing-Defects.png"
+          url="https://www.researchgate.net/figure/IBM-System-Science-Institute-Relative-Cost-of-Fixing-Defects_fig1_255965523"
+        />
+      </Section>
+      <Section level={3} title="Catégorisation des tests">
+        <Image
+          src="https://www.softwaretestinghelp.com/wp-content/qa/uploads/2007/08/Classification-of-Software-testing-types.png"
+          url="https://www.softwaretestinghelp.com/types-of-software-testing/"
+        />
+      </Section>
+      <Section level={2} title="(Non-)Fonctionnel">
+        <List
+          fragment={[false, true]}
+          items={[
+            [
+              "Test _fonctionnel_",
+              [
+                "Vérifie que le logiciel fait ce qu'il est censé faire selon les **spécifications fonctionnelles**",
+                "Exemples: unitaires, intégration, bout en bout",
+              ],
+            ],
+            [
+              "Test _non-fonctionnel_",
+              [
+                "Ne dépends pas des fonctionnalités du logiciel",
+                "Exemples: sécurité, performance",
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Types de tests">
+        <List
+          fragment
+          items={[
+            "Tests **unitaires**",
+            "Tests d'**intégration**",
+            "Tests de **bout en bout** (E2E)",
+            "Tests d'**acceptation**",
+            "Tests de **sécurité**",
+            "Tests de **performance**",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Tests unitaires">
+        <List
+          fragment
+          items={[
+            "Vérifie un **composant** isolé (fonction, classe, etc.)",
+            [
+              "Besoin de **mocks** pour simuler les dépendances",
+              [
+                "Mock au niveau de la **classe** ou de la **méthode**",
+                "Frameworks : Mockito (Java), unittest.mock (Python), etc.",
+              ],
+            ],
+            "Frameworks : JUnit (Java), Jest (JS), unittest (Python), etc.",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Tests d'intégration">
+        <List
+          fragment
+          items={[
+            "Vérifie l'**interaction** entre plusieurs composants",
+            [
+              "Appel à un **service** externe de test (mock)",
+              [
+                "Mock au niveau du **service** et de sa **réponse**",
+                "Connection à une **base de données** de test",
+              ],
+            ],
+            "Pour Spring (Java), démarrage d'un **contexte** de test",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Tests de bout en bout (end-to-end)">
+        <List
+          fragment
+          items={[
+            "Vérifie le **fonctionnement** de l'application en entier du point de vue de l'**utilisateur**",
+            "Simulation depuis le **navigateur**: Selenium",
+            "**Déploiement** de l'application sur un environnement de test",
+          ]}
+        />
+      </Section>
+      <Section level={4} title="Pyramide">
+        <Image
+          src="https://user.oc-static.com/upload/2019/08/17/15660719394662_P1CH1_pyramid.png"
+          url="https://openclassrooms.com/fr/courses/6100311-testez-votre-code-java-pour-realiser-des-applications-de-qualite/6440061-choisissez-les-bons-tests-automatises-avec-la-pyramide-de-tests"
+        />
+      </Section>
+      <Section level={4} title="Glace">
+        <Image
+          src="https://user.oc-static.com/upload/2019/08/18/15661075632134_P1CH1_cone_tests.png"
+          url="https://openclassrooms.com/fr/courses/6100311-testez-votre-code-java-pour-realiser-des-applications-de-qualite/6440061-choisissez-les-bons-tests-automatises-avec-la-pyramide-de-tests"
+        />
+      </Section>
+      <Section level={3} title="Tests d'acceptation">
+        <List
+          fragment
+          items={[
+            "Vérifie que le logiciel **répond aux besoins** du client",
+            "**Scénarios** de test",
+            "**Déploiement** de l'application sur un environnement accessible au client",
+            "**Rapport** de test",
+            "Proche du E2E mais orienté **métier**",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Tests de sécurité">
+        <List
+          fragment
+          items={[
+            "Vérifie que le logiciel n'est pas **vulnérable** aux attaques",
+            "Vérification des **dépendances** vulnérables : [OWASP Dependency Check](https://owasp.org/www-project-dependency-check/), [GitHub Dependabot](https://github.com/dependabot)",
+            "Analyse du **code source** : [SonarQube](https://www.sonarqube.org/), [GitLab SAST](https://docs.gitlab.com/ee/user/application_security/sast/)",
+            "Vérification des vulnérabilités connues sur un **déploiement** : [OWASP ZAP](https://owasp.org/www-project-zap/)",
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Tests de performance">
+        <List
+          fragment={[true, true]}
+          items={[
+            [
+              "Vérifie que le logiciel **répond aux exigences** de performance",
+              [
+                "**Load** (charge) : temps de réponse en fonction du nombre d'utilisateurs",
+                "**Endurance** : stabilité sur le long terme",
+                "**Stress** (surcharge) : stabilité en cas de surcharge",
+                "**Scale** : adaptation à une montée en charge",
+              ],
+            ],
+            "Déploiement sur un environnement dédié avec les même **ressources** que la production",
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Couverture de code">
+        <List
+          fragment
+          items={[
+            [
+              "Mesure le **pourcentage** de code couvert par les tests",
+              [
+                "[JaCoCo](https://www.eclemma.org/jacoco/) (Java), [Coverage.py](https://coverage.readthedocs.io/en/) (Python), etc.",
+              ],
+            ],
+            "Couverture des lignes, conditions, fonctions, etc.",
+            "Fixer un **objectif** de couverture : p. ex. 80%",
+            "Bonne couverture **≠** bons tests",
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Random testing">
+        <List
+          fragment
+          items={[
+            "Génération **aléatoire** de données de test",
+            "Vérification du résultat selon les **spécifications**",
+            "**Seed** pour pouvoir reproduire les tests en cas d'erreur",
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Test-driven development (TDD)">
+        <List
+          fragment={[true, true]}
+          items={[
+            [
+              "Écriture des tests **avant** le code",
+              [
+                "Ajouter un test",
+                "Lancer tous les tests et vérifier l'échec",
+                "Écrire le code",
+                "Lancer tous les tests et vérifier le succès",
+                "Refactor le code",
+              ],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Test-driven development (TDD)">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/TDD_Global_Lifecycle.png"
+          url="https://commons.wikimedia.org/wiki/File:TDD_Global_Lifecycle.png"
+        />
+      </Section>
+      <Section level={3} title="Tester ou ne pas tester, telle est la question">
+        <Image
+          src="https://www.commitstrip.com/wp-content/uploads/2013/04/Strips-Conscienses-du-test-550-final.jpg"
+          url="https://www.commitstrip.com/fr/2013/04/16/tester-ou-ne-pas-tester-telle-est-la-question/"
+        />
+      </Section>
     </RevealDeck>
   );
 }
